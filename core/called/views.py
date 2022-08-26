e83b45e7b902ca20931be0da2ae9b58ae750c639
41SergioF <carlossergio2401@outook.com>
2022-08-26 13:48:43 -0300

    New screen

--- core/called/views.py
+++ core/called/views.py
@@ -1,13 +1,18 @@
 
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
+
+from .models import Secretary
 
 def index(request):
     return render(request, 'called/index.html')
 
 def create(request):
-    return render(request, 'called/create.html')
+    list_secretary = Secretary.objects.all()
+    context = { 'list_secretary' : list_secretary}
+    return render(request, 'called/create.html', context)
 
 def query(request):
     return render(request, 'called/query.html')

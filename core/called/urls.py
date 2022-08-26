2ad9c35fb9b781aab2b4872a8807dc777c5180ae
41SergioF <carlossergio2401@outook.com>
2022-08-26 16:16:46 -0300

    Form does not work

--- core/called/urls.py
+++ core/called/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('criar/', views.create, name='create'),
+    path('criar/', views.create_page, name='create_page'),
+    path('criar/add/', views.create, name='create'),
     path('consultar/<int:call_id>/', views.query, name='query'),
 ]
--- core/called/views.py
+++ core/called/views.py
@@ -1,4 +1,7 @@
 
+import re
+from sre_constants import CALL
+from subprocess import call
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 # Create your views here.
@@ -8,10 +11,24 @@ from .models import Secretary, Call
 def index(request):
     return render(request, 'called/index.html')
 
-def create(request):
+def create_page(request):
     list_secretary = Secretary.objects.all()
     context = { 'list_secretary' : list_secretary}
     return render(request, 'called/create.html', context)
+
+def create(request):
+    print('create======================')
+    if request.method == 'POST':
+        call = Call()
+        call.secretary_sector=request.POST['secretary']
+        call.requester=request.POST['requester'] 
+        call.problem=request.POST['problem'] 
+
+        call.save()
+        
+        return HttpResponse("Postagem bem sucedida")
+    return HttpResponse("Método não permitido", status=403)
+    ''
 
 def query(request, call_id):
     call = get_object_or_404(Call, pk=call_id)
